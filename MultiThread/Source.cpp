e9a174d08102b6fc09ab79c068bd01637bcbb4d7
Vladislav <bakericecream@gmail.com>
2015-12-18 23:39:40 +0300

    complete. Changed output from console to file.

--- MultiThread/Source.cpp
+++ MultiThread/Source.cpp
@@ -1,13 +1,15 @@
 ﻿#include<thread>
-#include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
+#include<fstream>
 #include<io.h>
 #include<math.h>
 #include <mutex>
 #include <queue>
 #include <condition_variable>
 #include <random>
+#include<iomanip>
+
 using namespace std;
 
 mutex              lock_print;
@@ -17,10 +19,12 @@ mutex              lock_pr_buffer;
 condition_variable check_buffer;
 condition_variable check_ev_buffer;
 condition_variable check_pr_buffer;
-bool                    evaluation_done;
-bool                    notified_about_evaluation;
-bool                    printing_done;
-bool                    notified_about_printing;
+bool               evaluation_done;
+bool               notified_about_evaluation;
+bool               printing_done;
+bool               notified_about_printing;
+
+fstream logFile, valuesFile;
 
 class Point {
 public:
@@ -36,6 +40,34 @@ queue<Point*> buffer;
 queue<unsigned int> evaluatedTimesBuffer;
 queue<unsigned int> printedTimesBuffer;
 
+
+void PrintEvaluationToFile(double x, double y)
+{
+	valuesFile << fixed << setprecision(2) << "x: " << x << "; y: " << y << endl;
+}
+
+void LogToFile(unsigned int evaluationTime, unsigned int outputTime)
+{
+	logFile << "Evaluation time: " << evaluationTime << "milliseconds; file output time: " << outputTime << "milliseconds. " << endl;
+}
+
+void InitiateFiles()
+{
+	logFile.open("log.txt", ios::ate | ios::out);
+	valuesFile.open("values.txt", ios::ate | ios::out);
+}
+
+void CloseFiles()
+{
+	logFile.close();
+	valuesFile.close();
+}
+
+double EvaluateY(double x)
+{
+	return pow(x, 3);
+}
+
 void EvaluateMathFunc()
 {
 	{
@@ -45,11 +77,13 @@ void EvaluateMathFunc()
 
 	for (int i = 0; i < 10; i++)
 	{
-		double x = i + 1;
+		double x = i - 5;
 
 		unsigned int timeNowMS = clock();
-		Point* point = new Point(x, pow(x, 2));
+		double y = EvaluateY(x);
 		unsigned int lastEvaluatedTimeInMS = clock() - timeNowMS;
+
+		Point* point = new Point(x, y);
 
 		{
 			unique_lock<mutex> locker(lock_print);
@@ -59,11 +93,10 @@ void EvaluateMathFunc()
 		{
 			unique_lock<mutex> locker(lock_buffer);
 			buffer.push(point);
+			check_buffer.notify_all();
 
 			unique_lock<mutex> ev_locker(lock_ev_buffer);
 			evaluatedTimesBuffer.push(lastEvaluatedTimeInMS);
-
-			check_buffer.notify_all();
 			check_ev_buffer.notify_all();
 		}
 	}
@@ -85,11 +118,14 @@ void PrintInfoAboutEvaluation()
 			unsigned int timeNowMS = clock();
 			Point* tmpPoint = buffer.front();
 			buffer.pop();
+			check_buffer.notify_all();
+
+			PrintEvaluationToFile(tmpPoint->x, tmpPoint->y);
 			cout << "[printer] x: " << tmpPoint->x << "; y: " << tmpPoint->y << endl;
+
 			unique_lock<mutex> pr_buffer_locker(lock_pr_buffer);
 			unsigned int lastPrintedTimeInMS = clock() - timeNowMS;
 			printedTimesBuffer.push(lastPrintedTimeInMS);
-			check_buffer.notify_all();
 			check_pr_buffer.notify_all();
 		}
 	}
@@ -111,10 +147,12 @@ void PrintTiming()
 		while (!evaluatedTimesBuffer.empty() && !printedTimesBuffer.empty())
 		{
 			unique_lock<mutex> print_locker(lock_print);
+			LogToFile(evaluatedTimesBuffer.back(), printedTimesBuffer.back());
 			cout << "[time printer] eval time: " << evaluatedTimesBuffer.back() << "ms; file output time: " << printedTimesBuffer.back() << "ms. " << endl;
 			evaluatedTimesBuffer.pop();
-			printedTimesBuffer.pop();
 			check_ev_buffer.notify_all();
+
+			printedTimesBuffer.pop();
 			check_pr_buffer.notify_all();
 		}
 	}
@@ -122,6 +160,7 @@ void PrintTiming()
 
 int main()
 {
+	InitiateFiles();
 	thread timePrinterThread(PrintTiming);
 	thread loggerThread(PrintInfoAboutEvaluation);
 	thread evaluatingThread(EvaluateMathFunc);
@@ -130,5 +169,6 @@ int main()
 	loggerThread.join();
 	printing_done = true;
 	timePrinterThread.join();
+	CloseFiles();
 	return 0;
 }
